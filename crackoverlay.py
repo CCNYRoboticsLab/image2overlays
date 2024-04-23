9f58c414780f754aa88896f6e038ec1c271bfe54
Bo Shang <cnpcshangbo@gmail.com>
2024-04-23 12:48:55 -0400

    updated crackoverlay.py to use the filteredmasks and create filteredoverlay images

--- crackoverlay.py
+++ crackoverlay.py
@@ -8,9 +8,9 @@ config.read("config.ini")  # Replace with the actual path to your INI file
 
 # Paths for the mask images, raw images, and output images
 # Read from the INI file
-mask_dir = config["CrackSegmentation"]["mask_directory"]
+mask_dir = config["CrackSegmentation"]["mask_directory"].replace("crackmask", "filteredCrackMasks")
 raw_dir = config["Settings"]["image_path"]
-output_dir = config["CrackOverlay"]["overlay_directory"]
+output_dir = config["CrackOverlay"]["overlay_directory"].replace("crackoverlay", "filteredCrackOverlays")
 
 # Ensure the output directory exists
 if not os.path.exists(output_dir):
@@ -19,7 +19,7 @@ if not os.path.exists(output_dir):
 # Iterate over the files in the mask directory
 for mask_name in os.listdir(mask_dir):
     # Extract the image name from the filename
-    raw_name = mask_name.split(".")[0] + ".JPG"
+    raw_name = mask_name.split(".")[0] + ".jpg"
 
     # Load the mask and raw images
     mask = cv2.imread(os.path.join(mask_dir, mask_name), cv2.IMREAD_GRAYSCALE)
@@ -33,7 +33,7 @@ for mask_name in os.listdir(mask_dir):
     print(mask.shape)
 
     # Where the mask is white, set the raw image to red
-    raw[mask == 38] = [0, 0, 255]
+    raw[mask == 38] = [0, 255, 0]
 
     # Save the overlaid image to the output directory
     output_name = os.path.join(output_dir, raw_name)
